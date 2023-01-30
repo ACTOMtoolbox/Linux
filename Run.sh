bf39d3daa1d9ae6f9c1af5b8eb45cf2286ddf857
ACTOM-Dev <74307967+ACTOM-Dev@users.noreply.github.com>
2023-01-30 12:11:48 +0000

    Update

--- Run.sh
+++ Run.sh
@@ -3,11 +3,11 @@ TIMESTAMP=$(date +%Y%m%d%H%M%S)
 title=Run-$TIMESTAMP
 
 mkdir -p $title
-mkdir -p $title/logs
 cd $title
+
+#docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --mount type=bind,source="$(pwd)",target=/srv/actom-run/input actomtoolbox/actom-run
 
 docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --mount type=bind,source="$(pwd)",target=/srv/actom-run/input actomtoolbox/actom-run
 
-cp Run-All.sh logs/Run-All.sh
 sh Run-All.sh
 rm -r Run-All.sh
